5693937471c67b3b59ecee48e41138061d6841cc
Thorsten Brüning <thorsten@tb-net.de>
2015-05-19 00:47:40 +0200

    Initial Commit (TB)

--- src/arraytest/Arraytest.java
+++ src/arraytest/Arraytest.java
@@ -11,11 +11,56 @@ package arraytest;
  */
 public class Arraytest {
 
+    public static void eingabe() {
+        //Schleife bauen
+        //von der Eingabe (system.in() oder Scanner() einen int holen
+        // abbrechen wenn int < 0
+        // aufruf von function(en)
+        //schleifenende
+        
+        
+        
+        
+    }
+    
+     public static int[] feldnummer2index(int nr) {
+         int[] indexArray = {-1, -1};
+         int x=-1;
+         int y=-1;
+         
+         //rechnen
+         //
+         //
+         
+         
+         indexArray[0]= x;
+         indexArray[0]= y;
+         
+         return indexArray;
+     }
+    
+     
+     public static int index2feldnummer(int x, int y) {
+         int nr =-1;
+                  
+         //rechnen
+         //
+         //
+         
+                  
+         return nr;
+     }
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        eingabe();
+        
+        System.out.println("Fertig!");
     }
     
 }
